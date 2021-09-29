f9f936699165fb9fad1b6fa6da413c78027954e9
Marcela Tizzo Soares <marcelats.dev@gmail.com>
2021-09-29 02:42:23 -0300

    Changing DateTime validation

--- src/Alten.API/Models/Reservation.cs
+++ src/Alten.API/Models/Reservation.cs
@@ -4,8 +4,22 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 public class Reservation
 {
+    private DateTime accomodationStart;
+    private DateTime accomodationEnd;
+
     public int Id { get; set; }
+
     public string GuestName { get; set; }
-    public DateTime AccomodationStart { get; set; }
-    public DateTime AccomodationEnd { get; set; }
+
+    public DateTime AccomodationStart
+    {
+        get => accomodationStart;
+        set => accomodationStart = value.Date;
+    }
+
+    public DateTime AccomodationEnd
+    {
+        get => accomodationEnd;
+        set => accomodationEnd = value.Date.AddDays(1).AddSeconds(-1);
+    }
 }
--- src/Alten.API/Models/ReservationValidator.cs
+++ src/Alten.API/Models/ReservationValidator.cs
@@ -7,30 +7,22 @@ public class ReservationValidator : AbstractValidator<Reservation>
 {
     public ReservationValidator(IReservationService service)
     {
+
         RuleFor(r => r.GuestName)
-            .NotEmpty()
-            .WithMessage("The Guest Name is required.")
-            .Length(1, 300)
-            .WithMessage("The Guest Name cannot be more than 300 characters.");
+            .NotEmpty().WithMessage("The Guest Name is required.")
+            .Length(1, 300).WithMessage("The Guest Name cannot be more than 300 characters.");
 
         RuleFor(r => r.AccomodationStart)
-            .Cascade(CascadeMode.StopOnFirstFailure)
-            .NotNull()
-            .WithMessage("The Accomodation Start is required.")
-            .LessThanOrEqualTo(r => r.AccomodationEnd)
-            .WithMessage("The Accomodation Start should be lower than or equal to the Accomodation End")
-            .GreaterThan(DateTime.Now)
-            .WithMessage("The Accomodation should start at lead 1 day after the booking.")
-            .LessThanOrEqualTo(DateTime.Now.AddDays(30))
-            .WithMessage("The room cannot be reserved more than 30 days in advance.");
+            .Cascade(CascadeMode.Stop)
+            .NotNull().WithMessage("The Accomodation Start is required.")
+            .GreaterThan(DateTime.Now).WithMessage("The Accomodation should start at lead 1 day after the booking.")
+            .LessThanOrEqualTo(DateTime.Now.AddDays(30)).WithMessage("The room cannot be reserved more than 30 days in advance.");
 
         RuleFor(r => r.AccomodationEnd)
-            .Cascade(CascadeMode.StopOnFirstFailure)
-            .NotNull()
-            .GreaterThanOrEqualTo(r => r.AccomodationStart)
-            .WithMessage("The Accomodation End should be greater than or equal to the Accomodation End")
-            .LessThan(r => r.AccomodationStart.AddDays(3))
-            .WithMessage("The period of accomodation should be between 1 and 3 days");
+            .Cascade(CascadeMode.Stop)
+            .NotNull().WithMessage("The Accomodation End is required.")
+            .GreaterThanOrEqualTo(r => r.AccomodationStart).WithMessage("The Accomodation End should be greater than or equal to the Accomodation Start")
+            .LessThan(r => r.AccomodationStart.AddDays(3)).WithMessage("The period of accomodation should be between 1 and 3 days");
 
         RuleFor(r => r)
             .CustomAsync(async (reservation, context, cancelationToken) => 
